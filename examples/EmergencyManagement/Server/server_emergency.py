5d6ccb889a4d80019817916afd711fb3d61c212c
Corvo <60719165+brothercorvo@users.noreply.github.com>
2025-06-18 14:32:39 -0300

    Refactor emergency server to use dedicated service class

--- examples/EmergencyManagement/Server/server_emergency.py
+++ examples/EmergencyManagement/Server/server_emergency.py
@@ -1,28 +1,9 @@
 import asyncio
-from reticulum_openapi.service import LXMFService
-from examples.EmergencyManagement.Server.controllers_emergency import (
-    EmergencyController, EventController
-)
-from examples.EmergencyManagement.Server.models_emergency import (
-    EmergencyActionMessage,
-    Event,
-)
+from examples.EmergencyManagement.Server.service_emergency import EmergencyService
 
 
 async def main():
-    svc = LXMFService()
-    eamc = EmergencyController()
-    evc = EventController()
-    svc.add_route("CreateEmergencyActionMessage", eamc.CreateEmergencyActionMessage, EmergencyActionMessage)
-    svc.add_route("DeleteEmergencyActionMessage", eamc.DeleteEmergencyActionMessage)
-    svc.add_route("ListEmergencyActionMessage", eamc.ListEmergencyActionMessage)
-    svc.add_route("PatchEmergencyActionMessage", eamc.PatchEmergencyActionMessage, EmergencyActionMessage)
-    svc.add_route("RetrieveEmergencyActionMessage", eamc.RetrieveEmergencyActionMessage)
-    svc.add_route("CreateEvent", evc.CreateEvent, Event)
-    svc.add_route("DeleteEvent", evc.DeleteEvent)
-    svc.add_route("ListEvent", evc.ListEvent)
-    svc.add_route("PatchEvent", evc.PatchEvent, Event)
-    svc.add_route("RetrieveEvent", evc.RetrieveEvent)
+    svc = EmergencyService()
     svc.announce()
     service_task = asyncio.create_task(svc.start())
     try:
@@ -30,6 +11,7 @@ async def main():
     finally:
         await svc.stop()
         await service_task
+
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ examples/EmergencyManagement/Server/service_emergency.py
@@ -0,0 +1,39 @@
+from reticulum_openapi.service import LXMFService
+from examples.EmergencyManagement.Server.controllers_emergency import (
+    EmergencyController,
+    EventController,
+)
+from examples.EmergencyManagement.Server.models_emergency import (
+    EmergencyActionMessage,
+    Event,
+)
+
+
+class EmergencyService(LXMFService):
+    """Service with routes for the emergency management example."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        eamc = EmergencyController()
+        evc = EventController()
+
+        self.add_route(
+            "CreateEmergencyActionMessage",
+            eamc.CreateEmergencyActionMessage,
+            EmergencyActionMessage,
+        )
+        self.add_route("DeleteEmergencyActionMessage", eamc.DeleteEmergencyActionMessage)
+        self.add_route("ListEmergencyActionMessage", eamc.ListEmergencyActionMessage)
+        self.add_route(
+            "PatchEmergencyActionMessage",
+            eamc.PatchEmergencyActionMessage,
+            EmergencyActionMessage,
+        )
+        self.add_route("RetrieveEmergencyActionMessage", eamc.RetrieveEmergencyActionMessage)
+
+        self.add_route("CreateEvent", evc.CreateEvent, Event)
+        self.add_route("DeleteEvent", evc.DeleteEvent)
+        self.add_route("ListEvent", evc.ListEvent)
+        self.add_route("PatchEvent", evc.PatchEvent, Event)
+        self.add_route("RetrieveEvent", evc.RetrieveEvent)
